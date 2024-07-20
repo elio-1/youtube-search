fc96c276cbe816d7dad4018e11f1d0b5d05561c6
elio-1 <69924027+elio-1@users.noreply.github.com>
2024-07-20 18:51:16 +0200

    'clipboard+'

--- search.py
+++ search.py
@@ -1,11 +1,15 @@
 from pytube import Search
 import sys
+import pyperclip
 
 def get_url(video_name):
     return f"https://www.youtube.com/watch?v={Search(video_name).results[0].video_id}"
 
 def main():
-    print(get_url(' '.join(sys.argv[1:])))
+    url = get_url(' '.join(sys.argv[1:]))
+    print(url + ' copied into clipboard!' )
+    pyperclip.copy(url)
+
 
 if __name__ == "__main__":
     main()
